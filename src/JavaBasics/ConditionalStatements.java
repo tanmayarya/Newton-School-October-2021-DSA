88e41e71de3bc86e554360d2903e2b11698c8620
Tanmay Arya <tanmayarya@MacBook-Pro.local>
2021-10-13 23:45:04 +0530

    Java Operators

---
+++ src/JavaBasics/ConditionalStatements.java
@@ -0,0 +1,35 @@
+package JavaBasics;
+
+public class ConditionalStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		int age = 34;
+//		
+//		if(age > 18) {
+//			System.out.println("Minor");
+//		}else {
+//			System.out.println("Adult");
+//		}
+		
+		//grade system
+		// marks >= 90 print grade as A
+		// marks >= 70 and < 90 print grade as B
+//		marks >=50 and < 70 print grade C
+//		marks < 50 print D
+		
+		
+		// Z patern
+		
+//		System.out.println("*****");
+//		System.out.println("   *");
+//		System.out.println("  *");
+//		System.out.println(" *");
+//		System.out.println("*****");
+		System.out.println("*****\n   *\n  *\n *\n*****");
+		
+
+	}
+
+}
---
+++ src/JavaBasics/Operators.java
@@ -0,0 +1,64 @@
+package JavaBasics;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		
+		// unary operators
+		// increment
+		int a = 5;
+		int b = ++a;
+		int c = a++;
+		int d = ++c;
+		
+//		System.out.println("a = " + a);
+//		System.out.println("b = " + b);
+//		System.out.println("c = " + c);
+//		System.out.println("d = " + d);
+		
+		
+		//decrement
+		a = 5;
+		b = --a;
+		c = a--;
+		d = --c;
+		
+//		System.out.println("a = " + a);
+//		System.out.println("b = " + b);
+//		System.out.println("c = " + c);
+//		System.out.println("d = " + d);
+		a = 5;
+		b = 2;
+		c = 5;
+		d = 7;
+		
+		
+		// Relational Operators
+		// < , > , <= , >=, ==, !=
+//		System.out.println(a > b);
+//		System.out.println(a >= b);
+//		System.out.println(a < b);
+//		System.out.println(a != b);
+//		System.out.println(a == c);
+		
+		// logical operators
+		// && AND
+		// || OR
+//		System.out.println(true && true);
+//		System.out.println(false && true);
+//		System.out.println(false || true);
+//		System.out.println(a > b && c > b && a == c);
+
+		// Ternary operator ?:
+//		System.out.println(true ? "Hello" : "Hi");
+//		System.out.println(false ? "Hello" : "Hi");
+//		System.out.println(a < b ? "Hello" : "Hi");
+//		System.out.println(a > b ? '$' : 'f');
+//		System.out.println(a > b ? 1 : 0);
+		
+		
+		
+
+	}
+
+}
