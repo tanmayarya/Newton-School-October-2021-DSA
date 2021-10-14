5d7a452aa94194722c93c51812e92d23775470c3
Tanmay Arya <tanmayarya@MacBook-Pro.local>
2021-10-14 23:54:29 +0530

    conditionals and loops

--- src/JavaBasics/ConditionalStatements.java
+++ src/JavaBasics/ConditionalStatements.java
@@ -5,14 +5,14 @@ public class ConditionalStatements {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-//		int age = 34;
+		int age = 34;
 //		
 //		if(age > 18) {
 //			System.out.println("Minor");
 //		}else {
 //			System.out.println("Adult");
 //		}
-		
+//		
 		//grade system
 		// marks >= 90 print grade as A
 		// marks >= 70 and < 90 print grade as B
@@ -27,8 +27,57 @@ public class ConditionalStatements {
 //		System.out.println("  *");
 //		System.out.println(" *");
 //		System.out.println("*****");
-		System.out.println("*****\n   *\n  *\n *\n*****");
+//		System.out.println("*****\n   *\n  *\n *\n*****");
 		
+		//
+		// Print name of the day
+		int day = 7;
+		
+//		if(day == 1) {
+//			System.out.println("Monday");
+//		} else if(day == 2) {
+//			System.out.println("Tuesday");
+//		} else if(day == 3) {
+//			System.out.println("Wednesday");
+//		} else if(day == 4) {
+//			System.out.println("Thursday");
+//		} else if(day == 5) {
+//			System.out.println("Friday");
+//		} else if(day == 6) {
+//			System.out.println("Saturday");
+//		}
+//		else {
+//			System.out.println("Sunday");
+//		}
+		
+		
+		switch (4) {
+		case 1:
+			System.out.println("Monday");
+			break;
+		case 2: 
+			System.out.println("Tuesday");
+			break;
+		case 3: 
+			System.out.println("Wednesday");
+			break;
+		case 4: 
+			System.out.println("Thursday");
+			break;
+		case 5: 
+			System.out.println("Friday");
+			break;
+		
+		case 6: 
+			System.out.println("Saturday");
+			break;
+		case 7: 
+			System.out.println("Sunday");
+			break;
+		default:
+			System.out.println("Invalid day");
+			
+		}
 
 	}
 
---
+++ src/JavaBasics/Loops.java
@@ -0,0 +1,31 @@
+package JavaBasics;
+
+public class Loops {
+ public static void main(String[] args) {
+	
+	// loops
+	 
+	 // print number from 1 - 10;
+	 int i = 1;
+//	 while(i <= 10) {
+//		 System.out.println(i);
+//		 i++; // i = i+1
+//	 }
+//	 while(i <= 5) {
+//		 System.out.println(10);
+//		 i++;
+//	 }
+	 
+//	 do {
+//		System.out.println(i);
+//		i++;
+//	} while (i <= 10);
+	 
+	 
+	 for(i = 1; i <= 10; i++) {
+		 if(i%2 == 0) {
+			 System.out.println(i);
+		 }
+	 }
+}
+}
